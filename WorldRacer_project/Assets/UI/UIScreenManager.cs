7282ded7dcfab5090d4bd0d463deb69b3171b8bd
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-05-29 09:43:21 +0200

    Updated folder structure and started working on UI

---
+++ WorldRacer_project/Assets/UI/UIScreenManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScreenManager : MonoBehaviour
+{
+    public void ScreenIn() {
+
+    }
+
+    public void ScreenOut() {
+
+    }
+
+
+}
