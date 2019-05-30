0012e67f017a2f884c624c3f1dd1cef111bf1c71
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-05-30 11:34:22 +0200

    UIManager can now switch screens in a certain direction and destroy unused screens.

--- WorldRacer_project/Assets/UI/UIManager.cs
+++ WorldRacer_project/Assets/UI/UIManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 
 public class UIManager : MonoBehaviour
 {
+    public UIScreenManager currentScreen;
+
+    public UIScreenManager nextScreen;
+
     private Vector2 directionNext = new Vector2(-1, 0);
     private Vector2 directionPrevious = new Vector2(1, 0);
 
@@ -18,11 +21,28 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            SwitchScreen(nextScreen, directionNext, 0.5f);
+        }
     }
 
-    public void SwitchScreen(Vector2 direction)
+    public void SwitchScreen(UIScreenManager nextScreen, Vector2 direction, float duration)
     {
+        // Creates a new screen, animates this into the view and animates the current screen out of view
+        // The new screen is now the current screen
 
+        UIScreenManager nextScreenInstance = Instantiate(nextScreen, transform);
+        nextScreenInstance.Animate(-direction, direction, duration, false);
+        nextScreenInstance.uiManager = gameObject.GetComponent<UIManager>();
+
+        currentScreen.Animate(Vector2.zero, direction, duration, true);
+
+        currentScreen = nextScreenInstance;
+    }
+
+    public void DestroyScreen(UIScreenManager toDestroyScreen)
+    {
+        Destroy(toDestroyScreen.gameObject);
     }
 }
--- WorldRacer_project/Assets/UI/UIScreenManager.cs
+++ WorldRacer_project/Assets/UI/UIScreenManager.cs
@@ -1,14 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIScreenManager : MonoBehaviour
 {
     public RectTransform rectTransform;
     public Animation animationComponent;
+    public UIManager uiManager;
+    public Image image;
 
-    public void Animate(Vector2 position, float duration, Vector2 direction)
+    private void Start()
     {
+        image.color = Random.ColorHSV();
+    }
+
+    public void Animate(Vector2 position, Vector2 direction, float duration, bool destroyOnAnimationEnd)
+    {
+        // This function animates the screen from a certain position in a certain direction
+
         float width = rectTransform.rect.width;
         float height = rectTransform.rect.height;
 
@@ -16,7 +24,6 @@ public class UIScreenManager : MonoBehaviour
         {
             legacy = true
         };
-
 
         Keyframe[] keysX = new Keyframe[2];
         keysX[0] = new Keyframe(0.0f, position.x * width, 0.0f, 0.0f);
@@ -32,9 +39,21 @@ public class UIScreenManager : MonoBehaviour
         animationClip.SetCurve("", typeof(RectTransform), "m_AnchoredPosition.x", animationCurveX);
         animationClip.SetCurve("", typeof(RectTransform), "m_AnchoredPosition.y", animationCurveY);
 
+        if (destroyOnAnimationEnd)
+        {
+            AnimationEvent destroyEvent = new AnimationEvent();
+            destroyEvent.time = duration;
+            destroyEvent.functionName = "DestroyScreen";
+
+            animationClip.AddEvent(destroyEvent);
+        }
+
         animationComponent.AddClip(animationClip, animationClip.name);
         animationComponent.Play(animationClip.name);
+    }
 
-        Debug.Log(string.Format("Position: {0}, Direction: {1}", position, direction));
+    public void DestroyScreen()
+    {
+        uiManager.DestroyScreen(gameObject.GetComponent<UIScreenManager>());
     }
 }
