863e6e5e92e79e54a928a52ad424238af04b011d
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-05-31 16:38:02 +0200

    Added a circle slider Removed some example files from Mapbox

---
+++ WorldRacer_project/Assets/UI/UI Basic Elements/UICircleSlider.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UICircleSlider : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    public Image fillImage;
+    public RectTransform handleRect;
+
+    public float minValue;
+    public float maxValue;
+
+    public float currentvalue;
+
+    private float sliderRadius;
+
+    private int previousQuadrant;
+
+    public void Awake()
+    {
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        sliderRadius = rectTransform.rect.height / 2;
+
+        float initialAngle = (currentvalue / (maxValue - minValue)) * 360;
+        previousQuadrant = (int)initialAngle / 90;
+        UpdateValue(AngleToVector2(initialAngle));
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        UpdateValue(eventData.position);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        UpdateValue(eventData.position);
+    }
+
+    void UpdateValue(Vector2 pointerPosition) {
+
+        // Get angle in radians
+        Vector2 sliderPosition = transform.position;
+
+        float deltaY = pointerPosition.y - sliderPosition.y;
+        float deltaX = pointerPosition.x - sliderPosition.x;
+
+        float angle = Mathf.Atan2(deltaY, deltaX) * Mathf.Rad2Deg;
+
+        angle = (-angle + 450) % 360;
+
+        int currentQuadrant = (int)angle / 90;
+        if (Mathf.Abs(previousQuadrant - currentQuadrant) == 3) {
+            if (previousQuadrant == 0)
+            {
+                angle = 0;
+            }
+            if (previousQuadrant == 3)
+            {
+                angle = 360;
+            }
+
+        }
+        else
+        {
+            previousQuadrant = currentQuadrant;
+
+        }
+
+        float fillAmount = angle / 360;
+        fillImage.fillAmount = fillAmount;
+
+        handleRect.localPosition = AngleToVector2(angle) * sliderRadius;
+    }
+
+    Vector2 AngleToVector2(float angle)
+    {
+        float angleRad = (-angle + 90) * Mathf.Deg2Rad;
+        return new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+}
